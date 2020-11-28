5a5eee12eb781bd8dd267d8b721c5d1d8e46d98b
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:38:08 -0300

    Add files via upload

---
+++ EX-7-LISTA-1-Yohan_Aquino/EX-7-LISTA-1-Yohan_Aquino/Comparador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_7_LISTA_1_Yohan_Aquino
+{
+    class Comparador
+    {
+        private double n1;
+        private double n2;
+
+        public void setNum1(double x) 
+        {
+            n1 = x;
+        }
+        public void setNum2(double x)
+        {
+            n2 = x;
+        }
+        public double getMaior() 
+        {
+            if (n1 > n2) 
+            {
+                return n1;
+            }
+            else 
+            {
+                return n2;
+            }
+        }
+    }
+}
---
+++ EX-7-LISTA-1-Yohan_Aquino/EX-7-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_7_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*8. Entrar via teclado, com dois valores distintos. Exibir o maior deles. */
+            Comparador com = new Comparador();
+
+            Console.WriteLine("Digite o 1° valor");
+            com.setNum1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 2° valor");
+            com.setNum2(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("O maior valor é {0}", com.getMaior());
+            Console.ReadKey();
+
+        }
+    }
+}
